c988f19b59596f26f02e170459d2d71412d957ec
Ahiwamu <niceniceiloveyou@gmail.com>
2024-12-03 15:41:10 +0700

    Update lab7_2.cpp finished

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -5,27 +5,27 @@ using namespace std;
 
 int main() {
     string name, movie, day, text;
-    int key;
-    int gear;
-    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
-    cout << "?????: " ;
+    long long key;
+    long long gear;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name\?" << endl;
+    cout << "\?\?\?\?\?: " ;
     getline(cin,name);
     cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl;
-    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID\?" << endl;
     cout << name << ": " ;
     cin >> key;
     gear = (key-key%10000000)/10000000-12;
     cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you." << endl;
-    cout << "Fahsai: Let's go to the cinema together!!!" << endl << "Fahsai: What movie do you want to watch?" << endl;
+    cout << "Fahsai: Let's go to the cinema together!!!" << endl << "Fahsai: What movie do you want to watch\?" << endl;
     cout << name << ": " ;
     cin.ignore();
     getline(cin,movie);
-    cout << "Fahsai: So....which day are you free to go with me?" << endl;
+    cout << "Fahsai: So....which day are you free to go with me\?" << endl;
     cout << name << ": ";
     getline(cin,day);
-    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching" << movie << " with you." << endl;
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << endl;
     cout << name << ": " ;
     getline(cin,text);
-    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \(^ ^)/";
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\\(^ ^)/";
     return 0;
 }
